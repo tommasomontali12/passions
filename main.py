42227f8cf57febbc96dfd1cbb55e4623c4caaeb4
Tommaso Montali <102525459+tommasomontali12@users.noreply.github.com>
2024-09-27 19:01:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from random import choice as c
+argomenti = ["Ho pubblicato un gioco su itch.io fatto con Godot", "Sto lavorando ad una inteligenza artificiale con riconoscimento via fotocamera per scopo commerciale", "Preferisco i Google Pixel ai iPhone", "Mi piace avere un ecosistema professionale", "Trovo la programmazione come un lavoro"]
+
+print(c(argomenti))
